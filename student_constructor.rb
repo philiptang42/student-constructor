d8c513f97c77fa9da8c87009cc1670bf1ade0918
philiptang42 <philiptang42@gmail.com>
2016-01-11 01:24:16 -0500

    part 2 hash argument

--- student_constructor.rb
+++ student_constructor.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 class Student
-  attr_accessor :first_name, :last_name, :grades
+  attr_accessor :first_name, :last_name, :grades, :data
 
-  def initialize(first_name, last_name, grades)
-    @first_name = first_name
-    @last_name = last_name
-    @grades = grade
+  def initialize(data = {})
+    @first_name = data.values[0].to_s
+    @last_name = data.values[1].to_s
+    @grades = data.values[2].to_s
   end
 
 end
