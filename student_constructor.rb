98edf62236aa77a1ceefc95952b879f3e684f0f7
philiptang42 <philiptang42@gmail.com>
2016-01-11 01:41:07 -0500

    solution

--- student_constructor.rb
+++ student_constructor.rb
@@ -7,7 +7,7 @@ class Student
     @first_name = data.values[0].to_s
     @last_name = data.values[1].to_s
     @grades = data.values[2]
-    @average = @grades.inject{ |sum, el| sum + el }.to_f / @grades.size
+    @average = @grades.inject{|sum, el| sum + el}.to_f / @grades.size
   end
 
 end
